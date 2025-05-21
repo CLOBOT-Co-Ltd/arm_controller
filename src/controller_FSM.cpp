210bf37fdf894d7113c6e260626996de02e5f21f
J-DongHyeon <aiden3@clobot.co.kr>
2025-05-21 17:51:36 +0900

    debug

--- src/controller_FSM.cpp
+++ src/controller_FSM.cpp
@@ -15,9 +15,6 @@
 
 #include "arm_controller/controller_FSM.hpp"
 
-#include <chrono>
-#include <thread>
-
 
 ControllerFSM::ControllerFSM(
   std::shared_ptr<IArmController> arm_controller_node,
@@ -265,8 +262,6 @@ void ControllerFSM::initialize_FSM()
       arm_controller_node_->set_arm_motor_cmd(
         target_pos_array_, std::array<double, JOINT_NUMBER>{0.0f}, joint_kp_array_,
         joint_kd_array_, std::array<double, JOINT_NUMBER>{0.0f}, weight_);
-
-      std::this_thread::sleep_for(std::chrono::milliseconds(static_cast<int>(0.02 / 0.001f)));
 
 
       return true;
