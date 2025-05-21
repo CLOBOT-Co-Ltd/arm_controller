6e9588cb29a1a6dec496ffd5e30c47d148a0e414
J-DongHyeon <aiden3@clobot.co.kr>
2025-05-21 17:33:20 +0900

    debug

--- src/controller_FSM.cpp
+++ src/controller_FSM.cpp
@@ -246,17 +246,17 @@ void ControllerFSM::initialize_FSM()
 
         target_pos_array_[i] = angle_delta_per_control;
 
-        // std::cout << "Joint target" << i << ": " << target_pos_array_[i] << std::endl;
-        // std::cout << "wave hand pos " << i << ": " << wave_hand_pos_array_[i] * 180 / Pi <<
-        //   std::endl;
+        std::cout << "Joint target" << i << ": " << target_pos_array_[i] << std::endl;
+        std::cout << "wave hand pos " << i << ": " << wave_hand_pos_array_[i] * 180 / Pi <<
+          std::endl;
 
-        // std::cout << "current pos " << i << ": " << current_pos_array_[i] * 180 / Pi <<
-        //   std::endl;
-        // std::cout << "angle delta per control " << i << ": " <<
-        //   angle_delta_per_control * 180 / Pi <<
-        //   std::endl;
-        // std::cout << "target pos " << i << ": " << target_pos_array_[i] * 180 / Pi <<
-        //   std::endl;
+        std::cout << "current pos " << i << ": " << current_pos_array_[i] * 180 / Pi <<
+          std::endl;
+        std::cout << "angle delta per control " << i << ": " <<
+          angle_delta_per_control * 180 / Pi <<
+          std::endl;
+        std::cout << "target pos " << i << ": " << target_pos_array_[i] * 180 / Pi <<
+          std::endl;
       }
 
       arm_controller_node_->set_arm_motor_cmd(
